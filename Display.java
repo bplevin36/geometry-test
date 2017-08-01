6c595c913014cd2608602e951a628a2330c61f46
Ben Levin <blevin@akamai.com>
2017-08-01 10:53:39 -0700

    Added Shape list and drawing

--- Display.java
+++ Display.java
@@ -1,24 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Display extends JFrame{
+    ArrayList<Shape> shapes;
     public static void main(String[] args) {
         Display display = new Display();
-        dis
     }
         
     public Display(){
         super("Shape Visualizer");
+        shapes = new ArrayList<Shape>();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setBounds(100,100, 600, 400);
+        this.setBounds(100,100, 700, 500);
         this.setVisible(true);
         this.repaint();
     }
 
-    }
     public void paint(Graphics g){
-        super.paint(g);
-        g.setColor(Color.GREEN);
-        g.fillOval(25, 25, 100, 50);
+        for(Shape s : shapes){
+            if(s instanceof Circle){
+                g.fillOval(s.left(), s.top(), s.right()-s.left(), s.bottom()-s.top());
+            }else if(s instanceof Rectangle){
+                g.fillRect(s.left(), s.top(), s.right()-s.left(), s.bottom()-s.top());
+            }else if(s instanceof Polygon){
+                g.fillPolygon(((Polygon)s).getXCoords(), ((Polygon)s).getYCoords(), ((Polygon)s).getXCoords().length);
+            }
+        }
+    }
+
+    public void addShape(Shape a){
+        shapes.add(a);
     }
 }
